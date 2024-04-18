eaecc20bd0a4b47d73740f711decdb20fd5c7f72
Nikita <113856897+NGaidarenko@users.noreply.github.com>
2024-04-18 14:00:26 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -7,12 +7,17 @@ for _ in range(m):
     a, b = map(int, input().split())
     g[a - 1][b - 1] = True
 
+# Проверка на рефлексивность
 for i in range(0, n):
+    # Проверка на симметричность
     for j in range(i + 1, n):
+        # Проверка на транзетивность
+        # Цикл для создания побочной вершины соеденяющие i, j между собой
         for k in range(0, n):
+            # Проверка на то является ли побочная вершина i, j
             if k == i or k == j:
                 continue
-
+            # Проверка связи между вершинами i, j через вершину k
             if g[i][k] and g[k][j]:
                 transitive &= g[i][j]
                 antitransitive &= not g[i][j]
